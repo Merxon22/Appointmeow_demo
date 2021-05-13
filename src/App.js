05482a0eb7ff7d43ecf4b4b0275bd5c7efaa9442
Merxon22 <merxon22@gmail.com>
2021-05-13 17:45:17 +0800

    Connecting Flask & React

--- src/App.js
+++ src/App.js
@@ -5,23 +5,44 @@ import Friends from './pages/Friends.js';
 import Alarms from './pages/Alarms.js';
 import Register from './pages/Register.js';
 import Login from './pages/Login.js';
+import Test from './pages/test.js';
 import {
   HashRouter as Router,
   Route
 } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/setting" component={Setting} />
-      <Route exact path="/friends" component={Friends} />
-      <Route exact path="/alarms" component={Alarms} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-    </Router>
-  );
+  const [currentTime, setCurrentTime] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return(
+    <div>
+      <h1>Current time is {currentTime}.</h1>
+
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+    </div>
+  )
+
+  // return (
+  //   <Router>
+  //     <Route exact path="/" component={Home} />
+  //     <Route exact path="/setting" component={Setting} />
+  //     <Route exact path="/friends" component={Friends} />
+  //     <Route exact path="/alarms" component={Alarms} />
+  //     <Route exact path="/register" component={Register} />
+  //     <Route exact path="/login" component={Login} />
+  //     <Route exact path="/test" component={Test} />
+  //   </Router>
+  // );
 }
 
 export default App;
